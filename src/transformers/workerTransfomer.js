a65526e3951d4a4d7635430907ba436461eca0c6
Jerin Mathew <maxjerin@gmail.com>
2020-07-21 02:24:05 -0400

    Add deserializer for worker object. Update worker serializer method with contact type mapping

--- src/transformers/workerTransfomer.js
+++ src/transformers/workerTransfomer.js
@@ -1,3 +1,5 @@
+import _ from 'lodash';
+
 export const serializeWorker = worker => {
   return {
     firstName: worker.personalDetails.firstName,
@@ -15,6 +17,77 @@ export const serializeWorker = worker => {
         country: worker.address.country,
       },
     ],
+    contacts: [
+      {
+        type: 'CELL_PHONE',
+        value: '210-209-0912',
+        primary: worker.personalDetails.primaryContactType === 'CELL_PHONE',
+      },
+      {
+        type: 'HOME_PHONE',
+        value: '210-209-0912',
+        primary: worker.personalDetails.primaryContactType === 'HOME_PHONE',
+      },
+      {
+        type: 'WORK_PHONE',
+        value: '210-209-0912',
+        primary: worker.personalDetails.primaryContactType === 'WORK_PHONE',
+      },
+      {
+        type: 'EMAIL',
+        value: '210-209-0912',
+        primary: worker.personalDetails.primaryContactType === 'EMAIL',
+      },
+      {
+        type: 'SECONDARY_EMAIL',
+        value: '210-209-0912',
+        primary:
+          worker.personalDetails.primaryContactType === 'SECONDARY_EMAIL',
+      },
+    ],
+    testimony: worker.testimony,
+  };
+};
+
+export const deserializeWorker = worker => {
+  return {
+    personalDetails: {
+      firstName: worker.firstName,
+      middleName: worker.middleName,
+      lastName: worker.lastName,
+      dateOfBirth: worker.dateOfBirth,
+      gender: worker.gender,
+      cellPhone: worker.contacts
+        ? _.filter(worker.contacts, ['type', 'CELL_PHONE'])[0].value
+        : null,
+      homePhone: worker.contacts
+        ? _.filter(worker.contacts, ['type', 'HOME_PHONE'])[0].value
+        : null,
+      workPhone: worker.contacts
+        ? _.filter(worker.contacts, ['type', 'WORK_PHONE'])[0].value
+        : null,
+      primaryEmail: worker.contacts
+        ? _.filter(worker.contacts, ['type', 'EMAIL'])[0].value
+        : null,
+      secondaryEmail: worker.contacts
+        ? _.filter(worker.contacts, ['type', 'SECONDARY_EMAIL'])[0].value
+        : null,
+      primaryContactType: worker.contacts
+        ? _.filter(worker.contacts, ['primary', true])[0].type
+        : null,
+    },
+    address: {
+      addressLine1: worker.address[0].street,
+      addressLine2: worker.address[0].street2,
+      city: worker.address[0].city,
+      state: worker.address[0].state,
+      areaCode: worker.address[0].zipcode,
+      country: worker.address[0].country,
+    },
+    family: {
+      familyName: worker.family,
+      relationshipType: null,
+    },
     testimony: worker.testimony,
   };
 };
